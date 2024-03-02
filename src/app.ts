a1be5054be19117baa8d1b192d8f0b526c82a524
tri.dev <triquoc.dev@gmail.com>
2024-03-02 17:09:12 +0700

    create project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+
 
 const app: Express = express()
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, I am Trí, a full stack dev')
+    res.send('Hello, I am Trí, a full stack dev using Reactjs nodejs')
+})
+
+
+app.get('/123', (req: Request, res: Response) => {
+    res.send('123')
 })
 
 app.listen(port, () => {
