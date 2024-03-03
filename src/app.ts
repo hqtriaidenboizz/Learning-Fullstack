4747d39820e8efbb748ba01deee5d10b5fd901fa
tri.dev <triquoc.dev@gmail.com>
2024-03-04 02:30:36 +0700

    connect with mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,38 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"
+import mongoose from "mongoose";
 
 dotenv.config();
 
-
 const app: Express = express()
 const port = process.env.PORT || 8080;
+const mongo_url = process.env.MONGO_URL as string
+
+
+// middleware
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+//Connect database
+
+mongoose.Promise = Promise;
+mongoose.connect(mongo_url).then(() => console.log('Connected'))
+
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, I am Trí, a full stack dev using Reactjs nodejs')
+    res.send('Hello')
 })
 
-
-app.get('/123', (req: Request, res: Response) => {
-    res.send('123')
-})
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
